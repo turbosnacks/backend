68c817b7a7d17dc7f4d7259999d9bdad9e329afd
Sebastian Rollen <seb@overmu.se>
2022-03-08 10:28:23 -0500

    refactor to function and strong address type

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use axum::{routing::get, Router, Server};
-use std::net::SocketAddrV4;
+use std::net::{Ipv4Addr, SocketAddr};
+
+async fn hello_world() -> &'static str {
+    "Hello, World!"
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
-    let address: SocketAddrV4 = "0.0.0.0:3000".parse().unwrap();
+    let app = Router::new().route("/", get(hello_world));
+    let address = SocketAddr::new(Ipv4Addr::UNSPECIFIED.into(), 3000);
     Server::bind(&address.into())
         .serve(app.into_make_service())
         .await
