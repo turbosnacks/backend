30ca9324246e55dbd10a5fcd15725de379c77f97
Sebastian Rollen <seb@overmu.se>
2022-03-08 10:25:18 -0500

    make hello world route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router, Server};
+use std::net::SocketAddrV4;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let address: SocketAddrV4 = "0.0.0.0:3000".parse().unwrap();
+    Server::bind(&address.into())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
